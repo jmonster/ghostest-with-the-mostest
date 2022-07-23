c75051ddab6a49da0605d7841242079f152b02d2
Johnny Domino <jvdomino@gmail.com>
2022-07-22 22:08:13 -0400

    Stub out client and serve it via node

---
+++ server.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const pathToHTML = path.join(__dirname, "client.html");
+const PORT = 3000 || process.env.PORT;
+
+const server = http.createServer(function (req, res) {
+  const { size } = fs.statSync(pathToHTML);
+  const contentStream = fs.createReadStream(pathToHTML);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": size,
+  });
+  contentStream.pipe(res);
+});
+
+server.listen(PORT);
+
+console.log(`Serving ${pathToHTML} at http://localhost:${PORT}`);
