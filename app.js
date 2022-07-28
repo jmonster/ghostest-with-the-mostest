88528754245ee689d2dfddc9983734503131edbd
Johnny Domino <jvdomino@gmail.com>
2022-07-28 16:29:10 -0400

    add CORS support

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const logger = require("morgan");
 const commentsRouter = require("./routes/comments");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
